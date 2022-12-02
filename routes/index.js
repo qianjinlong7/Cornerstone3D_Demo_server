85e47e5508abf921788f44254d70098022d03eb9
钱锦龙 <qianjinlong@radynhealth.com>
2022-12-02 17:59:56 +0800

    为获取职位列表做了初步设计

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const connection = require('../db/db_connect')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
+
+router.post('/getPosition', (req, res) => {
+  const { curPage, pageSize } = req.body
+  const low = (curPage - 1) * pageSize
+  const SQLstr = 'SELECT * from position order by positionNo limit ' + low + "," + pageSize
+
+  connection.query(SQLstr, (error, results) => {
+    if (!error) {
+      console.log(results)
+      res.send(results)
+    } else {
+      throw error
+    }
+  })
+})
 
 module.exports = router;
