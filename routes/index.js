58bd41e4c27cf17d12ed9013518fb207d4bb136d
钱锦龙 <qianjinlong@radynhealth.com>
2022-12-06 14:45:43 +0800

    获取职位列表和媒体列表功能完成

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,41 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
-router.post('/getPosition', (req, res) => {
-  const { curPage, pageSize } = req.body
-  const low = (curPage - 1) * pageSize
-  const SQLstr = 'SELECT * from position order by positionNo limit ' + low + "," + pageSize
+router.get('/getPosition', (req, res) => {
+  const SQLstr = 'SELECT * from position order by positionNo;'
 
   connection.query(SQLstr, (error, results) => {
     if (!error) {
-      console.log(results)
       res.send(results)
     } else {
       throw error
     }
   })
+})
+
+router.post('/getMedia', (req, res) => {
+  const { curPage, pageSize } = req.body
+  const low = (curPage - 1) * pageSize
+  const SQLstr1 = 'SELECT * from media order by mediaNo desc limit ' + low + "," + pageSize + ';'
+  const SQLstr2 = 'SELECT * from media;'
+  const data = []
+
+  connection.query(SQLstr1, (error, results) => {
+    if (!error) {
+      data.push(results)
+      connection.query(SQLstr2, (error, results) => {
+        if (!error) {
+          data.push(results.length)
+          res.send(data)
+        } else {
+          throw error
+        }
+      })
+    } else {
+      throw error
+    }
+  })
+
 })
 
 module.exports = router;
